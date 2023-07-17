b9a1a75eaa91a037b9241972b38ca01d95cba421
Peter Aldhous <peter@peteraldhous.com>
2023-07-17 08:38:16 -0700

    Include geocoding of locations, update README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Driving directions
 
 Teaching demonstration of how to make a web app using Mapbox APIs and Turf.js to give driving distance, driving time, and route to the nearest of several points of interest loaded from GeoJSON. The app will first try to find the user's location and start the route from there. Users can also enter a start location into the search box.
+
+The file `server.py` is provided to launch a local web server to view the app offline. At the command line, navigate to the folder containing these files and then run `python server.py`. In your web browser, then navigate to `http://localhost:8000/` and the app should launch. (Note: If you don't launch a local server to view offline, your browser's Cross-Origin Resource Sharing policy will likely prevent loading of the local GeoJSON file.)
+
+View the app online [here](https://paldhous.github.io/driving-directions-app/).
--- locations.R
+++ locations.R
@@ -4,17 +4,22 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # load required packages
 library(tidyverse)
 library(sf)
+library(tidygeocoder)
 
 # load locations
 locations <- read_csv("locations.csv")
 
-# convert to sf object
+# geocode, if you don't already have latitude and longitude (note, set address to be the name of the column with the address in your data)
+locations <- geocode(locations, address = address, method = "arcgis", full_results = TRUE)
+
+# convert to sf object 
 locations <- locations %>%
-  st_as_sf(coords = c("longitude","latitude"),
+  st_as_sf(coords = c("long","lat"), # use names for columns with lat and long if you have already and don't need geocoding step
            crs = st_crs("EPSG:4326"))
 
+
 locations <- locations %>%
-  select(address = desc)
+  select(address)
 
 # write to geojson
 st_write(locations, "locations.geojson", delete_dsn = TRUE)
